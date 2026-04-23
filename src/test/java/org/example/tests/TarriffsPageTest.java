3bfd6969f6b663252ce4773f3202612a0ee62e60
Test User <jefferyasamoahbless@gmail.com>
2026-04-23 14:21:06 +0000

    test: Add TarriffsPageTest for tests on tarrifs page

---
+++ src/test/java/org/example/tests/TarriffsPageTest.java
@@ -0,0 +1,33 @@
+package org.example.tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import org.example.pages.AvailabilityCheckPage;
+import org.example.pages.TarriffsPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Epic("Telekom Tariff Page")
+@Feature("Tariff Page Functionality")
+public class TarriffsPageTest extends BaseTest {
+
+    @Test
+    @Story("Navigate to tariff page and verify elements")
+    @Description("Test the tariff page display and navigation after availability check")
+    public void testTariffPage() {
+        AvailabilityCheckPage availabilityPage = new AvailabilityCheckPage();
+        TarriffsPage tarriffsPage = new TarriffsPage();
+
+        availabilityPage.acceptCookies();
+        availabilityPage.enterPlzOrderStadt("34277, Fuldabrück, Fuldabrück");
+        availabilityPage.enterStrasse("Ahornweg");
+        availabilityPage.enterHausnummer("1");
+        availabilityPage.clickAdresse();
+
+        Assert.assertTrue(tarriffsPage.isWirPlanenDisplayed(), "'Wir planen' should be displayed");
+
+        tarriffsPage.clickNein();
+    }
+}
