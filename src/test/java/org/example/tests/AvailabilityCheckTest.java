5c85d643efdcc9f5cc4bcbfdd8714ec9991ddff8
Test User <jefferyasamoahbless@gmail.com>
2026-04-23 15:04:15 +0000

    Add TarriffsPageTest to testng.xml suite

--- src/test/java/org/example/tests/AvailabilityCheckTest.java
+++ src/test/java/org/example/tests/AvailabilityCheckTest.java
@@ -5,9 +5,6 @@ import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.example.pages.AvailabilityCheckPage;
-import org.example.pages.OptionsPage;
-import org.example.pages.PlanningPage;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 @Epic("Telekom Availability Check")
@@ -19,19 +16,11 @@ public class AvailabilityCheckTest extends BaseTest {
     @Description("Test the availability check flow from entering address to viewing options")
     public void testAvailabilityCheck() {
         AvailabilityCheckPage availabilityPage = new AvailabilityCheckPage();
-        PlanningPage planningPage = new PlanningPage();
-        OptionsPage optionsPage = new OptionsPage();
 
         availabilityPage.acceptCookies();
         availabilityPage.enterPlzOrderStadt("34277, Fuldabrück, Fuldabrück");
         availabilityPage.enterStrasse("Ahornweg");
         availabilityPage.enterHausnummer("1");
         availabilityPage.clickAdresse();
-
-        Assert.assertTrue(planningPage.isWirPlanenDisplayed(), "'Wir planen' should be displayed");
-
-        planningPage.clickNein();
-
-        Assert.assertTrue(optionsPage.isMeineMoglichkeitenDisplayed(), "'Meine Möglichkeiten' should be displayed");
     }
 }
