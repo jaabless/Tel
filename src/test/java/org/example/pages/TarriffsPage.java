ef16c21f7960e3a5d9cc655f87361330426d68b7
Test User <jefferyasamoahbless@gmail.com>
2026-04-23 15:04:05 +0000

    Add radio button and product button steps to TarriffsPage

--- src/test/java/org/example/pages/TarriffsPage.java
+++ src/test/java/org/example/pages/TarriffsPage.java
@@ -9,6 +9,8 @@ public class TarriffsPage extends BasePage {
 
     private SelenideElement wirPlanenText = $(By.cssSelector("[data-qa='changeAddressBlockTitle']"));
     private SelenideElement neinButton = $(By.cssSelector("scale-button[data-qa='newCustomerButton']"));
+    private SelenideElement neuenGlasfaserRadio = $(By.cssSelector("scale-radio-button[data-qa='newCustomerNewConnectionOption']"));
+    private SelenideElement verfuegbareProdukteButton = $(By.cssSelector("[data-qa='buttonCheckAvailability']"));
 
     public boolean isWirPlanenDisplayed() {
         logAction("Checking if 'Wir planen' is displayed");
@@ -25,5 +27,15 @@ public class TarriffsPage extends BasePage {
     public void clickNein() {
         logAction("Clicking Nein button");
         neinButton.click();
+    }
+
+    public void selectNeuenGlasfaserAnschluss() {
+        logAction("Selecting 'Neuen Glasfaser-Anschluss' radio button");
+        neuenGlasfaserRadio.click();
+    }
+
+    public void clickVerfuegbareProdukte() {
+        logAction("Clicking 'Verfügbare Produkte anzeigen' button");
+        verfuegbareProdukteButton.click();
     }
 }
