366fe7264118176e6633aab5181471daa9c78ebd
Test User <jefferyasamoahbless@gmail.com>
2026-04-24 11:51:45 +0000

    Add AVM, MagentaTV click methods and Zur button returning CheckoutPage

--- src/test/java/org/example/pages/HardwarePage.java
+++ src/test/java/org/example/pages/HardwarePage.java
@@ -7,10 +7,13 @@ import org.openqa.selenium.By;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class HardwarePage extends BasePage {
 
     private SelenideElement bitteWaehlenTitle = $(By.cssSelector("[data-qa='pageTitle-basic']"));
+    private SelenideElement avmRouter = $(By.cssSelector("[data-qa='hardwarefritzbox5590-private']"));
+    private SelenideElement magentaTV = $(By.cssSelector("[data-qa='hardwaremagentatvone_v2_bf']"));
 
     public boolean isBitteWaehlenDisplayed() {
         logAction("Checking if 'Bitte wählen' is displayed");
@@ -20,5 +23,23 @@ public class HardwarePage extends BasePage {
             logAction("'Bitte wählen' title not found: " + e.getMessage());
             return false;
         }
+    }
+
+    public void clickAvm() {
+        logAction("Clicking AVM router option");
+        avmRouter.shouldBe(Condition.visible).click();
+    }
+
+    public void clickMagentaTV() {
+        logAction("Clicking MagentaTV option");
+        magentaTV.shouldBe(Condition.visible).click();
+    }
+
+    public CheckoutPage clickZur() {
+        logAction("Clicking 'Zur' scale-button");
+        executeJavaScript(
+            "Array.from(document.querySelectorAll('scale-button')).find(el => el.textContent.trim().startsWith('Zur')).click();"
+        );
+        return new CheckoutPage();
     }
 }
