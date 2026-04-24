b48b034982f01ab282986440ab6ff1a06382280c
Test User <jefferyasamoahbless@gmail.com>
2026-04-24 14:25:58 +0000

    Simplify HardwarePageTest using TestHelper.navigateToHardwarePage

--- src/test/java/org/example/tests/HardwarePageTest.java
+++ src/test/java/org/example/tests/HardwarePageTest.java
@@ -6,8 +6,6 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.example.pages.CheckoutPage;
 import org.example.pages.HardwarePage;
-import org.example.pages.ProductsPage;
-import org.example.pages.TarriffsPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,21 +17,7 @@ public class HardwarePageTest extends BaseTest {
     @Story("Navigate to hardware page and select hardware options")
     @Description("Test hardware selection and checkout navigation after tariff selection")
     public void testHardwarePage() {
-        // Navigate to tariff page and complete tariff selection
-        TarriffsPage tarriffsPage = new TestHelper().getTariffPageAfterNavigation();
-
-        // Navigate through tariff options
-        tarriffsPage.clickNein();
-        tarriffsPage.selectNeuenGlasfaserAnschluss();
-        tarriffsPage.clickVerfuegbareProdukte();
-
-        ProductsPage productsPage = new ProductsPage();
-        productsPage.clickPrivatkunden();
-        productsPage.selectGlasfaser2000();
-        productsPage.selectGlasfaser2000MitMagentaTV();
-
-        // Navigate to hardware page
-        HardwarePage hardwarePage = productsPage.clickWeiter();
+        HardwarePage hardwarePage = new TestHelper().navigateToHardwarePage();
         Assert.assertTrue(hardwarePage.isBitteWaehlenDisplayed(), "'Bitte wählen' should be displayed");
 
         // Select hardware options
