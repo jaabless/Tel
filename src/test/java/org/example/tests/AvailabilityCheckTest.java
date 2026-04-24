aa2f70f080ca43536032e322b849407f3dec00ae
Test User <jefferyasamoahbless@gmail.com>
2026-04-24 14:04:17 +0000

    Use TestHelper in AvailabilityCheckTest to remove duplicated navigation logic

--- src/test/java/org/example/tests/AvailabilityCheckTest.java
+++ src/test/java/org/example/tests/AvailabilityCheckTest.java
@@ -4,7 +4,6 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
-import org.example.pages.AvailabilityCheckPage;
 import org.testng.annotations.Test;
 
 @Epic("Telekom Availability Check")
@@ -15,12 +14,6 @@ public class AvailabilityCheckTest extends BaseTest {
     @Story("Check availability and navigate through options")
     @Description("Test the availability check flow from entering address to viewing options")
     public void testAvailabilityCheck() {
-        AvailabilityCheckPage availabilityPage = new AvailabilityCheckPage();
-
-        availabilityPage.acceptCookies();
-        availabilityPage.enterPlzOrderStadt("34277, Fuldabrück, Fuldabrück");
-        availabilityPage.enterStrasse("Ahornweg");
-        availabilityPage.enterHausnummer("1");
-        availabilityPage.clickAdresse();
+        new TestHelper().getTariffPageAfterNavigation();
     }
 }
