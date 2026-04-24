0f3e67d5d271679991066a3c43fd98e62d15b3df
Test User <jefferyasamoahbless@gmail.com>
2026-04-24 14:25:54 +0000

    Add navigateToHardwarePage helper to centralise tariff-to-hardware navigation

--- src/test/java/org/example/tests/TestHelper.java
+++ src/test/java/org/example/tests/TestHelper.java
@@ -1,6 +1,8 @@
 package org.example.tests;
 
 import org.example.pages.AvailabilityCheckPage;
+import org.example.pages.HardwarePage;
+import org.example.pages.ProductsPage;
 import org.example.pages.TarriffsPage;
 
 public class TestHelper extends BaseTest {
@@ -27,5 +29,19 @@ public class TestHelper extends BaseTest {
     public TarriffsPage getTariffPageAfterNavigation(TestData.Address address) {
         navigateToTariffPage(address);
         return new TarriffsPage();
+    }
+
+    public HardwarePage navigateToHardwarePage() {
+        TarriffsPage tarriffsPage = getTariffPageAfterNavigation();
+        tarriffsPage.clickNein();
+        tarriffsPage.selectNeuenGlasfaserAnschluss();
+        tarriffsPage.clickVerfuegbareProdukte();
+
+        ProductsPage productsPage = new ProductsPage();
+        productsPage.clickPrivatkunden();
+        productsPage.selectGlasfaser2000();
+        productsPage.selectGlasfaser2000MitMagentaTV();
+
+        return productsPage.clickWeiter();
     }
 }
