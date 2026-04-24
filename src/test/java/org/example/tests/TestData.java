6ab24753858d6f889353a3165e73dc1f69f5a31a
Test User <jefferyasamoahbless@gmail.com>
2026-04-24 14:13:12 +0000

    Add TestData class with default address constants

---
+++ src/test/java/org/example/tests/TestData.java
@@ -0,0 +1,35 @@
+package org.example.tests;
+
+public class TestData {
+    public static final String DEFAULT_PLZ = "34277";
+    public static final String DEFAULT_STADT = "Fuldabrück";
+    public static final String DEFAULT_STRASSE = "Ahornweg";
+    public static final String DEFAULT_HAUSNUMMER = "1";
+
+    public static final String PLZ_ORDER_STADT = DEFAULT_PLZ + ", " + DEFAULT_STADT + ", " + DEFAULT_STADT;
+
+    public static class Address {
+        private final String plz;
+        private final String stadt;
+        private final String strasse;
+        private final String hausnummer;
+
+        public Address(String plz, String stadt, String strasse, String hausnummer) {
+            this.plz = plz;
+            this.stadt = stadt;
+            this.strasse = strasse;
+            this.hausnummer = hausnummer;
+        }
+
+        public String getPlzOrderStadt() {
+            return plz + ", " + stadt + ", " + stadt;
+        }
+
+        public String getStrasse() { return strasse; }
+        public String getHausnummer() { return hausnummer; }
+
+        public static Address defaultAddress() {
+            return new Address(DEFAULT_PLZ, DEFAULT_STADT, DEFAULT_STRASSE, DEFAULT_HAUSNUMMER);
+        }
+    }
+}
